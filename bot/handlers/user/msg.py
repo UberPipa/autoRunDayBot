2d1c51af555e091659a4ee99559de61eec8556d0
UberPipa <nu.takoe.b@gmail.com>
2023-06-13 12:00:01 +0300

    Сделал грамотное открытие дня

--- bot/handlers/user/msg.py
+++ bot/handlers/user/msg.py
@@ -1,7 +1,8 @@
 from aiogram import Dispatcher, types
-from bot.startEndDay.actions.actions import reopen_day, close_day
+from bot.startEndDay.actions.actions import reopen_day, close_day, open_day
 from bot.startEndDay.actions.statusWork import getting_start
 from bot.database.models.users import Users
+import datetime
 
 
 async def openReopen_day(msg: types.Message) -> None:
@@ -18,7 +19,14 @@ async def openReopen_day(msg: types.Message) -> None:
         if status['STATE'] == 'OPENED':
             await msg.answer(text='Рабочи день уже идёт')
         else:
-            await reopen_day(session, csrf)
+            # Проверяем когда был последний старт дня, если сегодня, то применяем reopen, если нет, то open
+            last_date_start = datetime.datetime.fromtimestamp(int(status['INFO']['DATE_START']))
+            last_date_start = last_date_start.date()
+            today = datetime.date.today()
+            if last_date_start == today:
+                await reopen_day(session, csrf)
+            else:
+                await open_day(session, csrf)
             await msg.answer(text='Рабочий день начат')
     else:
         await msg.answer(text='Неверно указан логин или пароль')
